a241286768189db92f0afc21407b17ed2574f13d
Stephen Radford <steve228uk@gmail.com>
2017-11-22 17:41:17 +0000

    Update dependencies

--- SRBarcodeScanner/SRBarcodeScannerDelegate.swift
+++ SRBarcodeScanner/SRBarcodeScannerDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  SRBarcodeScannerDelegate.swift
-//  SellFormula
+//  SRBarcodeScanner
 //
 //  Created by Stephen Radford on 10/08/2015.
 //  Copyright (c) 2015 Cocoon Development Ltd. All rights reserved.
@@ -11,15 +11,15 @@ import Foundation
 @objc public protocol SRBarcodeScannerDelegate {
     
     /// Should the device fail to create the input (probably because permission wasn't granted), this method will be called.
-    optional func failedToCreateDeviceInputWithError(error: NSError)
+    @objc optional func failedToCreateDeviceInputWithError(error: NSError)
     
     /// Scanning has started running successfully
-    optional func scanningStarted()
+    @objc optional func scanningStarted()
     
     /// Scanning has stopped running successfully
-    optional func scanningStopped()
+    @objc optional func scanningStopped()
     
     /// Called when a barcode has been found
-    optional func foundBarcode(barcode: String)
+    @objc optional func foundBarcode(barcode: String)
     
 }
